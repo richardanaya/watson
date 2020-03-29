ae72a2e5cdbc86cc7c56ffff6b2f032436dee1ec
Richard <Richard>
2020-03-29 04:48:30 -0700

    better colors

--- example/visualizer/src/main.rs
+++ example/visualizer/src/main.rs
@@ -2,14 +2,15 @@ use std::env;
 use std::fs::File;
 use std::io::prelude::*;
 use watson::*;
+use colored::*;
 
 fn print_type_section(s: &TypeSection) {
-    println!("- Types");
+    println!("  [{}]","Type".purple());
     for i in 0..s.types.len() {
         match &s.types[i] {
             WasmType::Function(f) => {
                 println!(
-                    "  - Type 0: function(inputs{:?}) -> outputs{:?}",
+                    "  0: fn(inputs{:?}) -> outputs{:?}",
                     f.inputs, f.outputs
                 );
             }
@@ -18,48 +19,48 @@ fn print_type_section(s: &TypeSection) {
 }
 
 fn print_function_section(s: &FunctionSection) {
-    println!("- Functions");
+    println!("  [{}]","Function".purple());
     for i in 0..s.function_types.len() {
-        println!("  - Function {}: Type {:?}", i, s.function_types[i]);
+        println!("  {}: type[{:?}]", i, s.function_types[i]);
     }
 }
 
 fn print_export_section(s: &ExportSection) {
-    println!("- Exports");
+    println!("  [{}]","Export".purple());
     for i in 0..s.exports.len() {
         match &s.exports[i] {
             WasmExport::Function(f) => {
-                println!("  - {:?} Function {}", f.name, f.index);
+                println!("  {:?} function[{}]", f.name, f.index);
             }
             WasmExport::Memory(f) => {
-                println!("  - {:?} Memory {}", f.name, f.index);
+                println!("  {:?} memory[{}]", f.name, f.index);
             }
         }
     }
 }
 
 fn print_memory_section(s: &MemorySection) {
-    println!("- Memories");
+    println!("  [{}]","Memory".purple());
     for i in 0..s.memories.len() {
         println!(
-            "  - Memory {}: min {} max {}",
+            "  {}: min {} max {}",
             i, s.memories[i].min_pages, s.memories[i].max_pages
         );
     }
 }
 
 fn print_code_section(s: &CodeSection) {
-    println!("- Code");
+    println!("  [{}]","Code".purple());
     for i in 0..s.code_blocks.len() {
         println!(
-            "  - Function {}: locals{:?} code{:?}",
+            "  {}: locals{:?} code{:?}",
             i, s.code_blocks[i].locals, s.code_blocks[i].code
         );
     }
 }
 
 fn print_unknown_section(s: &UnknownSection) {
-    println!("- UnknownSection[{}]", s.id);
+    println!("  [{}:{}]", "Unknown".purple(), s.id);
     println!("  {:?}", s.data);
 }
 
@@ -85,9 +86,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     f.read_to_end(&mut buffer)?;
 
     let program = Program::load(&buffer)?;
-    println!("Program: {}", &args[1]);
+    println!("{} {{", &args[1].green());
     for s in program.sections.iter() {
         print_section(&s);
     }
+    println!("}}");
     Ok(())
 }
