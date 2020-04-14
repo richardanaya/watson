a1c28cfd732fe051f6d269e08c06b7b05002c2e6
Richard <Richard>
2020-04-13 18:37:40 -0700

    preparing for next thing to work on

--- examples/bfi/src/main.rs
+++ examples/bfi/src/main.rs
@@ -3,27 +3,29 @@ use std::{env, error::Error, fs, process::exit};
 use watson::*;
 
 fn run(program: impl InterpretableProgram) -> Result<Vec<WasmValue>, &'static str> {
-    let mut interpreter = Interpreter::new(program);
+    let mut interpreter = Interpreter::new(program)?;
     let mut executor = interpreter.call("main", &[])?;
     loop {
         let execution_unit = executor.next()?;
-        let response: ExecutionResponse = match execution_unit {
+        let response = match execution_unit {
             // if an import is called, figure out what to do
             ExecutionUnit::CallImport(x) => {
                 if x.name == "output_byte" {
-                    let char_code = [x.params[0].to_i32() as u8];
-                    let text = from_utf8(&char_code).unwrap();
-                    print!("{}", text);
+                    let b = x.params[0].to_i32() as u8;
+                    let chars = [b];
+                    let text = from_utf8(&chars).unwrap();
+                    println!("{}", text);
                     ExecutionResponse::DoNothing
-                } else if x.name == "input_byte" {
-                    ExecutionResponse::Values(vec![42.to_wasm_value()])
+                } else if x.name == "import_byte" {
+                    // TODO
+                    ExecutionResponse::DoNothing
                 } else {
-                    panic!("not sure what to do")
+                    panic!("unknown import call")
                 }
             }
             // if there's nothing left to do, break out of loop
             ExecutionUnit::Complete(v) => break Ok(v),
-            // handle default
+            // handle other execution units with default behavior
             mut x @ _ => x.evaluate()?,
         };
         executor.execute(response)?;
